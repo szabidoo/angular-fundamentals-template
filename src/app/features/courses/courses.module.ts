d894527ae63dd22167cd0b7b973bf91462efee08
szabidoo <szabidoo0814@gmail.com>
2025-06-29 14:24:51 +0200

    fixing search component

--- src/app/features/courses/courses.module.ts
+++ src/app/features/courses/courses.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CoursesComponent } from "./courses.component";
 import { SharedModule } from "@app/shared/shared.module";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [CoursesComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, FormsModule],
 })
 export class CoursesModule {}
